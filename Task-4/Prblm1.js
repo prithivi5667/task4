b2bcbf28ee2baa0756d79ef7ad509c6dbe58e12e
prithivi5667 <77000089+prithivi5667@users.noreply.github.com>
2021-03-21 09:59:00 -0200

    Add files via upload

---
+++ Task-4/Prblm1.js
@@ -0,0 +1,18 @@
+//1.Create a request variable
+var request =new XMLHttpRequest();
+//2.Create a connection.
+//Open has three parameter(get method,url,boolean)
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+//3.Send the request
+request.send();
+//4.Process and Load the response
+request.onload = function(){  //function name not necessary coz attached to event
+  let data=JSON.parse(this.response);
+  
+  let asiaRegion=data.filter((item) =>{
+  if(item.region==='Asia'){ 
+     console.log(item);
+  }
+  });
+
+}
---
+++ Task-4/Prblm4.js
@@ -0,0 +1,17 @@
+//1.Create a request variable
+var request =new XMLHttpRequest();
+//2.Create a connection.
+//Open has three parameter(get method,url,boolean)
+request.open('GET','https://restcountries.eu/rest/v2/all',true);
+//3.Send the request
+request.send();
+//4.Process and Load the response
+request.onload = function(){  //function name not necessary coz attached to event
+  let data=JSON.parse(this.response);
+  
+  let totalpopulation=data.reduce((acc,item)=>{
+    return  acc+item.population;
+   });
+   console.log(totalpopulation);
+
+}
